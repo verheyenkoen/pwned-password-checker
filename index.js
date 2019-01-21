6077f13890fc195a09f18abdcd6f61bc8218554e
Koen Verheyen <verheyen.koen@gmail.com>
2019-01-21 13:35:34 +0100

    First working version using fast-csv, sha1 and node-fetch libraries

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import yargs from 'yargs'
+import csv from 'fast-csv'
+import sha1 from 'sha1'
+import fetch from 'node-fetch'
 
 const args = yargs
   .command(
@@ -27,4 +30,38 @@ const args = yargs
   .alias('h', 'help')
   .help().argv
 
-console.log(args)
+csv
+  .fromPath(args.file, { headers: true, ignoreEmpty: true, trim: true })
+  .on('data', record => {
+    if (record[args.password]) {
+      // skip empty passwords
+      const hash = sha1(record[args.password]).toUpperCase()
+      const prefix = hash.substring(0, 5)
+      const suffix = hash.substring(5)
+
+      fetch(`https://api.pwnedpasswords.com/range/${prefix}`)
+        .catch(err => {
+          debugger
+          console.error(err)
+        })
+        .then(res => res.text())
+        .then(data => {
+          csv
+            .fromString(data, { delimiter: ':', headers: ['suffix', 'count'] })
+            .on('data', data => {
+              if (suffix === data.suffix) {
+                console.log(
+                  `The password for "${record[args.name]}" was found ${
+                    data.count
+                  } time(s).`
+                )
+                console.log(
+                  `  User: ${record[args.username]} / Password: ${
+                    record[args.password]
+                  }`
+                )
+              }
+            })
+        })
+    }
+  })
