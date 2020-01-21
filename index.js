6e3b7c211c8498d9d197aa80e5d018c03f5b1886
Koen Verheyen <verheyen.koen@gmail.com>
2020-01-21 16:16:41 +0100

    Added logging at end of check

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ import sha1 from 'sha1'
 import fetch from 'node-fetch'
 
 const args = yargs
-  .command(
-    '$0 <file>',
-    'Check a CSV file for breached passwords against HaveIBeenPwned.'
-  )
+  .command('$0 <file>', 'Check a CSV file for breached passwords against HaveIBeenPwned.')
   .option('u', {
     alias: 'username',
     default: 'username',
@@ -30,10 +27,14 @@ const args = yargs
   .alias('h', 'help')
   .help().argv
 
+let count = 0
+
 csv
   .fromPath(args.file, { headers: true, ignoreEmpty: true, trim: true })
   .on('data', record => {
     if (record[args.password]) {
+      count++
+
       // skip empty passwords
       const hash = sha1(record[args.password]).toUpperCase()
       const prefix = hash.substring(0, 5)
@@ -46,22 +47,13 @@ csv
         })
         .then(res => res.text())
         .then(data => {
-          csv
-            .fromString(data, { delimiter: ':', headers: ['suffix', 'count'] })
-            .on('data', data => {
-              if (suffix === data.suffix) {
-                console.log(
-                  `The password for "${record[args.name]}" was found ${
-                    data.count
-                  } time(s).`
-                )
-                console.log(
-                  `  User: ${record[args.username]} / Password: ${
-                    record[args.password]
-                  }`
-                )
-              }
-            })
+          csv.fromString(data, { delimiter: ':', headers: ['suffix', 'count'] }).on('data', data => {
+            if (suffix === data.suffix) {
+              console.log(`The password for "${record[args.name]}" was found ${data.count} time(s).`)
+              console.log(`  User: ${record[args.username]} / Password: ${record[args.password]}`)
+            }
+          })
         })
     }
   })
+  .on('end', rowCount => console.log(`${count} password(s) checked from ${rowCount} rows.`))
